db0c0fa065878921d58926fc5cf2975aeaf95fc1
Vinicius <fsvinicius9@gmail.com>
2022-04-29 18:48:42 -0300

    challenge completed

--- 22_hora_de_praticar_template/app.js
+++ 22_hora_de_praticar_template/app.js
@@ -4,11 +4,15 @@ new Vue({
     nome: 'Vinicius Farias',
     idade: 21,
     linkHtml: '<a href="http://google.com"></a>',
-    linkImage: 'https://www.google.com/search?q=zoro&rlz=1C1FCXM_pt-PTBR995BR995&sxsrf=ALiCzsa30IG7T_gcEa0MGrrHSLIS5ZXHWA:1651268380066&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjH2eOJnrr3AhX2D7kGHZ7lBXUQ_AUoAXoECAIQAw&biw=1920&bih=969&dpr=1#imgrc=ekbSGKk2XBVcfM'
+    linkImage: 'https://ovicio.com.br/wp-content/uploads/2021/07/20210712-one-piece-zoro-wano-postcover-1024x576.jpg'
   },
   methods: {
       randomNumber : function(){
         return Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    }
+    ,
+    alterarInput: function(event){
+      this.nome = event.target.value
     }
   } // metodos que acontecem em eventos no html
 })
